f5a2f6c3e428dd93bec9980a996d93f990ccc43d
Maphikza <stapisi@protonmail.com>
2023-02-10 17:58:27 +0200

    Adding file clean up after action

--- main.py
+++ main.py
@@ -2,18 +2,32 @@ import pytesseract
 from PIL import Image
 from pathlib import Path
 import pyperclip
+import shutil
+from datetime import datetime
 import os
+import time
 
-image_path = Path(os.environ.get("IMAGE_PATH"))
-text_list = []
-for img in image_path.iterdir():
-    print(img)
-    image = Image.open(img)
-    pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-    text = pytesseract.image_to_string(image)
-    text_list.append(text)
 
-required = " ".join(map(str, text_list))
+def read_image_text() -> str:
+    image_path = Path(os.environ.get("IMAGE_PATH"))
+    text_list = []
+    for img in image_path.iterdir():
+        print(img)
+        image = Image.open(img)
+        pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+        text = pytesseract.image_to_string(image)
+        text_list.append(text)
+    required = " ".join(map(str, text_list))
+    return required
+
+
+def save_to_file(content: str):
+    file_name = "result.txt"
+    image_path = Path(os.environ.get("IMAGE_PATH"))/file_name
+    txt_file = image_path
+    txt_file.parent.mkdir(parents=True, exist_ok=True)
+    with txt_file.open('w') as f:
+        f.write(content)
 
 
 def copy_text(some_text: str):
@@ -21,6 +35,26 @@ def copy_text(some_text: str):
     print("Text has been copied to clipboard.")
 
 
-copy_text(required)
+def move_files(src, dest):
+    try:
+        shutil.move(src, dest)
+    except Exception as e:
+        print("An error occurred: ", e)
 
-print(required)
+
+result = read_image_text()
+save_to_file(result)
+copy_text(result)
+
+src_dir = Path(os.environ.get("IMAGE_PATH"))
+dest_dir = Path(os.environ.get("DEST_PATH"))
+current_date = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+dest_folder = dest_dir / current_date
+dest_folder.mkdir(parents=True, exist_ok=True)
+
+files = src_dir.iterdir()
+time.sleep(1)
+for file in files:
+    src_file = file
+    dest_file = dest_folder / file.name
+    move_files(src_file, dest_file)
